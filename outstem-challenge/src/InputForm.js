b7c85dd0b7b8ebe1e7e6755ca8d8984c4daba7fa
Philippe Gagné <philg2000@gmail.com>
2021-05-24 11:38:54 -0400

    implemented login error checking in console log

--- outstem-challenge/src/InputForm.js
+++ outstem-challenge/src/InputForm.js
@@ -15,37 +15,56 @@ class InputForm extends React.Component {
   
     handleSubmit(event) {
 
-        var email_address = email.state.value;
-        var pw = password.state.value;
-        var check = document.getElementById("terms_conditions").checked;
+        const email_value = this.emailInput.state.value;
+        const password_value = this.passwordInput.state.value;
+        const check = document.getElementById("terms_conditions").checked;
+        console.log(check); 
+        console.log(email_value);
+        console.log(password_value);
         
-        console.log(email_address);
-
-
-    //   alert('A name was submitted: ' + this.state.value);
-    //   event.preventDefault();
+        if(email_value==""){
+            console.log("Email cannot be empty");
+        } else if (!this.validateEmail(email_value)){
+            console.log("This doesn't look like an email, please try again");
+        } else if (password_value==""){
+            console.log("Password cannot be empty");
+        } else if (password_value.length < 8){
+            console.log("Your password must be 8 characters or longer");
+        } else if (!check){
+            console.log("You must agree to the Terms of Service and Privacy Policy before proceeding");
+        } else {
+            console.log("Login succesful");
+        }
+        
+        event.preventDefault();
     }
 
+    validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
   
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
+            <a href="https://www.google.com/" target="_blank">
             <button className="google-button"><img src={google_logo} alt="Google logo" className="button-icon"></img>Continue with Google</button>
+            </a>
 
             <div className="strike">
                 <span>or sign in with email</span>
             </div>
 
           <div className="input-component"> 
-            <InputField name="email" type="email"  label="Email" />
+            <InputField name="email" type="email"  label="Email" ref={(ref) => this.emailInput = ref}/>
           </div>
 
           <div className="input-component"> 
-            <InputField name= "password" type="password" label="Password (min. 8 characters)"/>
+            <InputField name= "password" type="password" label="Password (min. 8 characters)" ref={(ref) => this.passwordInput = ref}/>
           </div> 
 
             <div className="input-component">
-            <input type="checkbox" id="terms_conditions" name="terms_conditions" value="Terms and Conditions" />
+            <input type="checkbox" id="terms_conditions" name="terms_conditions" />
                 <label for="terms_conditions">I agree to the <a href="">Terms of Service</a> and <a href="">Privacy Policy</a></label><br />
             </div>
 
